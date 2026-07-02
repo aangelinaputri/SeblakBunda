5b83b7a21cb7c356af9c1995a6d57079209f2705
aangelinaputri <angelinaaputrii5@gmail.com>
2026-07-02 18:47:45 +0700

    up new

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        //
+        $this->call([
+            AdminSeeder::class,
+            OrderSeeder::class,
+            PengeluaranSeeder::class,
+        ]);
     }
 }
